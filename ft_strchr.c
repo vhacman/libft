01cf20fabc21966e132cc88a538baa836c311b92
Viorica Gabriela Hacman <vhacman@e3r10p5.42roma.it>
2025-01-27 15:23:09 +0100

    ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,9 +6,12 @@
 /*   By: vhacman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 14:41:29 by vhacman           #+#    #+#             */
-/*   Updated: 2025/01/27 14:47:38 by vhacman          ###   ########.fr       */
+/*   Updated: 2025/01/27 14:56:20 by vhacman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/* searches for the first occurence of a character C in string S.
+It returns a pointer to the matched charachter if found, or NULL if the char
+is not found. If C is the termiantor, it returns a pointer to it */
 
 #include "libft.h"
 
