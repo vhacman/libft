3efe45cc6daa2ffce0d203307ad3571e33eb7780
gabdev98 <gabdev98@MSI.>
2025-01-29 18:57:05 +0100

    norminette

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,15 +16,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	i = 0;
 	if (n == 0)
 		return (0);
-    if (i == n);
+	if (i == n)
 		return (0);
-	i = 0;
 	while (i < n - 1 && s1[i] != '\0' && s1[i] == s2[i])
 		i++;
-	
-		
 	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
 }
 /*
