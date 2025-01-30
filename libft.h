9b1ece4dd885db34e39f4bebce6e3c2d64a591ed
gabdev98 <gabdev98@MSI.>
2025-01-30 15:56:07 +0100

    tolte funzioni non necessarie

--- libft.h
+++ libft.h
@@ -18,36 +18,31 @@
 # include <unistd.h>
 # include <stdio.h>
 
+int	ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *b, int c, size_t count);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	ft_putchar(char c);
-void	ft_putstr(char *s);
-void	ft_putnbr(int n);
-char	*ft_strcpy(char *dest, const char *src);
-char	*ft_strncpy(char *dest, char *src, size_t n);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_strdup(const char *s1);
-char	*ft_strcat(char *s1, const char *s2);
-char	*ft_strncat(char *dest, char *src, size_t nb);
-size_t	ft_strlcat(char *dest, const char *src, size_t destsize);
-int		ft_strcmp(const char *s1, const char *s2);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strstr(char *str, char *to_find);
-size_t	ft_strlen(const char *str);
-int		ft_atoi(const char *str);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isprint(int c);
+void	*ft_memset(void *b, int c, size_t count);
 void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
 void    ft_putendl_fd(char *s, int fd);
 void    ft_putnbr_fd(int n, int fd);
-void    ft_putendl(char *s);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcat(char *dest, const char *src, size_t destsize);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
 #endif
