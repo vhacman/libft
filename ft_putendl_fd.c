69575d6979930ca0925b024c9a8e91d6abe83d46
gabdev98 <gabdev98@MSI.>
2025-01-30 20:36:22 +0100

    corrette

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -12,12 +12,12 @@
 
 #include "libft.h"
 
-void    ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-        if (!s)
-                return ;
-        ft_putstr_fd(s, fd);
-        ft_putchar_fd('\n', fd);
+	if (!s)
+		return ;
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 
 }
 /*
