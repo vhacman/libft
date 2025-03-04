42f64ea75ebd7a0cd635241030e2ab0f8c255cfc
Gabri <hacmanvioricagabriela@gmail.com>
2025-03-04 13:18:25 +0100

    implementazione bonus

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -13,14 +13,14 @@
 #include "libft.h"
 
 //function that creates a newnode
-t_list  *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-    t_list  *new_node;
+	t_list	*new_node;
 
-    new_node = (t_list *)malloc(sizeof(t_list));
-    if(!new_node)
-        return (NULL);
-    new_node->content = content;
-    new_node->next = NULL;
-    return(new_node);
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
 }
--- libft.h
+++ libft.h
@@ -61,5 +61,5 @@ void	*ft_calloc(size_t number, size_t size);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-
+t_list  *ft_lstnew(void *content);
 #endif
